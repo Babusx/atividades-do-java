104a5d12f89019e1dfa170de595b7171719a946e
guilherme de oliveira macedo <go479724@gmail.com>
2023-02-06 09:40:38 -0300

    arividade jav

---
+++ atv.js
@@ -0,0 +1,15 @@
+function clicou(){
+    document.querySelector("h1").innerText = "Fui clicaso "
+
+}
+
+function entrou(){
+    document.querySelector("div").innerText = "Mouse está aqui" 
+
+}
+
+function saiu(){
+    document.querySelector("div").innerText = "mouse saiu "
+    
+} 
+  
