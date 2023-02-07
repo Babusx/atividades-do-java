b8149ce250cead32fc32afb3eeec935f2c382e15
guilherme de oliveira macedo <go479724@gmail.com>
2023-02-07 11:26:51 -0300

    Classlist

---
+++ Classlist.js
@@ -0,0 +1,11 @@
+function ClickMe(){
+
+   document.querySelector("#minhaDiv").classList.add("classe1","classe2","classe3")
+
+}
+
+function clicou(){
+    document.querySelector("#minhaDiv").classList.remove("classe1","classe2","classe3")
+
+}
+
---
+++ elementos.js
@@ -0,0 +1,10 @@
+
+function clickMe(){
+
+    const paragrafo = document.createElement(`p`)
+    paragrafo.innerText = "É O TRIKAS"
+    
+    document.body.appendChild(paragrafo)
+
+}
+    
